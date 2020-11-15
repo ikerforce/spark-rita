c994adeb28c97f48b7507a4e1ac5994e40bc74c1
ikerforce <iker.mt@gmail.com>
2020-11-14 18:51:10 -0600

    Eliminé el código de spark innecesario.

--- src/calculo_demoras_rutas/rita_dask_demoras_en_rutas.py
+++ src/calculo_demoras_rutas/rita_dask_demoras_en_rutas.py
@@ -27,7 +27,8 @@ user = creds["user"] # Usuario de mysql
 password = creds["password"] # Password de mysql
 database = creds["database"] # Base de datos en la que almaceno resultados y tiempo de ejecucion
 time_table = config["time_table"] # Tabla en la que almaceno el tiempo de ejecucion de la tarea
-results_table = config["results_table"] # Tabla en la que almaceno el resultado
+results_table_aeropuertos = config["results_table_aeropuertos"] # Tabla en la que almaceno el resultado
+results_table_mkt = config["results_table_mkt"] # Tabla en la que almaceno el resultado
 input_table = config["input_table"] # Tabla con los datos necesarios para la ejecucion del algoritmo
 time_table_mode = config["time_table_mode"] # Forma en la que escribo el resultado en la tabla (append o overwrite)
 results_table_mode = config["results_table_mode"] # Forma en la que escribo el resultado en la tabla (append o overwrite)
@@ -50,24 +51,25 @@ df = dd.read_sql_table(input_table, uri=uri, index_col=partition_col)
 # EJECUCION
 # ----------------------------------------------------------------------------------------------------
 df['ROUTE_AIRPORTS'] = df.apply(lambda row: row['ORIGIN'] + '-' + row['DEST'], axis=1, meta='str') # Agregamos una columna que sea el concatenado de el origen y destino para así tener una ruta
-print(df['ROUTE_AIRPORTS'].compute().head())
-# Cálculo de demoras en el origen
+df['ROUTE_MKT_ID'] = df.apply(lambda row: row['ORIGIN_MKT_ID'] + '-' + row['DEST_MKT_ID'], axis=1, meta='str') # Agregamos una columna que sea el concatenado de el origen y destino de mkt_id para así tener una ruta
+
+# Cálculo de demoras en rutas agrupadas por aeropuerto
 agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
 
-lista_df = utils.rollup(df, ['ORIGIN', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+lista_df = utils.rollup(df, ['ROUTE_AIRPORTS', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
 
-lista_df[0].to_sql(results_table_origen, uri, if_exists='replace', index=False) # En la primera escritura borro los resultados anteriores
+lista_df[0].to_sql(results_table_aeropuertos, uri, if_exists='replace', index=False) # En la primera escritura borro los resultados anteriores
 for resultado in lista_df[1:]:
-    resultado.to_sql(results_table_origen, uri, if_exists='append', index=False) # Despupes solo hago append
+    resultado.to_sql(results_table_aeropuertos, uri, if_exists='append', index=False) # Despupes solo hago append
 
-# Cálculo de demoras en el destino
+# Cálculo de demoras agrupadas por mkt_id
 agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
 
-lista_df = utils.rollup(df, ['DEST', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+lista_df = utils.rollup(df, ['ROUTE_MKT_ID', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
 
-lista_df[0].to_sql(results_table_destino, uri, if_exists='replace', index=False) # En la primera escritura borro los resultados anteriores
+lista_df[0].to_sql(results_table_mkt, uri, if_exists='replace', index=False) # En la primera escritura borro los resultados anteriores
 for resultado in lista_df[1:]:
-    resultado.to_sql(results_table_destino, uri, if_exists='append', index=False) # Despupes solo hago append
+    resultado.to_sql(results_table_mkt, uri, if_exists='append', index=False) # Despupes solo hago append
 
 t_final = time.time() # Tiempo de finalizacion de la ejecucion
 # ----------------------------------------------------------------------------------------------------
@@ -78,67 +80,4 @@ t_final = time.time() # Tiempo de finalizacion de la ejecucion
 info_tiempo = [[t_inicio, t_final, t_final - t_inicio, exec_desc, resources, time.strftime('%Y-%m-%d %H:%M:%S')]]
 df_tiempo = pd.DataFrame(data=info_tiempo, columns=['start_ts', 'end_ts', 'duration', 'description', 'resources', 'insertion_ts'])
 df_tiempo.to_sql(time_table, uri, if_exists=time_table_mode, index=False)
-# ----------------------------------------------------------------------------------------------------
-
-
-
-
-
-# DEFINICION DE FUNCIONES
-# ----------------------------------------------------------------------------------------------------
-def principales_rutas_fecha(df):
-    """Esta funcion calcula el retraso promedio en la salida, llegada y la duracion promedio para cada ruta.
-    Los resultados se presentan para cada dia (DAY), cada mes (MONTH), cada trimestre (QUARTER) y cada ano (YEAR).
-    \nLa entrada es un dataframe que contiene los datos de lugar, fecha, duracion y retraso de cada vuelo."""
-    # Obtencion de ruta por dia
-    df_resp = df.groupBy('ORIGIN_CITY_MARKET_ID', 'DEST_CITY_MARKET_ID','ORIGIN', 'DEST', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH', 'FL_DATE')\
-        .agg(F.count("FL_DATE").alias("N_FLIGHTS"), F.avg('ARR_DELAY').alias("ARR_DELAY"), F.avg('DEP_DELAY').alias("DEP_DELAY"), F.avg('ACTUAL_ELAPSED_TIME').alias("ACTUAL_ELAPSED_TIME"))\
-        .withColumn('ROUTE_AIRPORTS', F.array('ORIGIN', 'DEST'))\
-        .withColumn('ROUTE_MKT_ID', F.array('ORIGIN_CITY_MARKET_ID', 'DEST_CITY_MARKET_ID'))
-    
-    # Calculo de indicadores por dia (DAY), cada mes (MONTH), cada trimestre (QUARTER) y cada ano (YEAR)
-    df_resp = df_resp.rollup('ROUTE_MKT_ID', 'ROUTE_AIRPORTS', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH')\
-        .agg(F.grouping_id().alias('GROUPING_ID'), F.count("FL_DATE").alias("N_FLIGHTS"), F.avg('ARR_DELAY').alias("AVG_ARR_DELAY"), F.avg('DEP_DELAY').alias("AVG_DEP_DELAY"), F.avg('ACTUAL_ELAPSED_TIME').alias("AVG_ACTUAL_ELAPSED_TIME"))\
-        .orderBy(F.desc('GROUPING_ID'))\
-        .withColumn('ORIGIN', F.expr('ROUTE_AIRPORTS[0]'))\
-        .withColumn('DEST', F.expr('ROUTE_AIRPORTS[1]'))\
-        .withColumn('ORIGIN_MKT_ID', F.expr('ROUTE_MKT_ID[0]'))\
-        .withColumn('DEST_MKT_ID', F.expr('ROUTE_MKT_ID[1]'))\
-        .drop('ROUTE_AIRPORTS')\
-        .drop('ROUTE_MKT_ID')
-    return df_resp
-# ----------------------------------------------------------------------------------------------------
-
-
-# EJECUCION
-# ----------------------------------------------------------------------------------------------------
-df_resp = principales_rutas_fecha(df_rita) # Calculo de demoras en cada ruta
-df_resp.write.format("jdbc")\
-    .options(
-        url=db_url + database,
-        driver=db_driver,
-        dbtable=results_table,
-        user=user,
-        password=password,
-        numPartitions=db_numPartitions)\
-    .mode(results_table_mode)\
-    .save()
-t_final = time.time() # Tiempo de finalizacion de la ejecucion
-# ----------------------------------------------------------------------------------------------------
-
-
-# REGISTRO DE TIEMPO
-# ----------------------------------------------------------------------------------------------------
-rdd_time = sc.parallelize([[t_inicio, t_final, t_final - t_inicio, exec_desc, resources]]) # Almacenamos infomracion de ejecucion en rdd
-df_time = rdd_time.toDF(['start_ts', 'end_ts', 'duration', 'description', 'resources'])\
-    .withColumn("insertion_ts", F.current_timestamp())
-df_time.write.format("jdbc")\
-    .options(
-        url=db_url + database,
-        driver=db_driver,
-        dbtable=time_table,
-        user=user,
-        password=password)\
-    .mode(time_table_mode)\
-    .save()
 # ----------------------------------------------------------------------------------------------------
