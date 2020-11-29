470c983bcc2f42c5933e8478c85ad12453786e5e
ikerforce <iker.mt@gmail.com>
2020-11-29 17:58:45 -0600

    Agregué un archivo de spark que hará todas las ejecuciones.

---
+++ src/rita_master_spark.py
@@ -0,0 +1,109 @@
+# PREPARACION DE AMBIENTE
+# ----------------------------------------------------------------------------------------------------
+# Importaciones de PySpark
+from pyspark import SparkContext
+sc = SparkContext()
+from pyspark.sql import SparkSession, SQLContext
+spark = SparkSession(sc)
+sqlContext = SQLContext(sc)
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# Importaciones de Python
+import argparse # Utilizado para leer archivo de configuracion
+import json # Utilizado para leer archivo de configuracion
+import time # Utilizado para medir el timpo de ejecucion
+
+# Al ejecutar el archivo se debe de pasar el argumento --config /ruta/a/archivo/de/crecenciales.json
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", help="Ruta hacia archivo de configuracion")
+parser.add_argument("--creds", help="Ruta hacia archivo con credenciales de la base de datos")
+args = parser.parse_args()
+# Leemos las credenciales de la ruta especificada
+with open(args.config) as json_file:
+    config = json.load(json_file)
+with open(args.creds) as json_file:
+    creds = json.load(json_file)
+# ----------------------------------------------------------------------------------------------------
+
+
+# LECTURA DE DATOS
+# ----------------------------------------------------------------------------------------------------
+t_inicio = time.time() # Inicia tiempo de ejecucion
+
+# Lectura de datos de MySQL
+df_rita = spark.read.format("jdbc")\
+    .options(
+        url=creds["db_url"] + creds["database"],
+        driver=creds["db_driver"],
+        dbtable="(SELECT * FROM " + config["input_table"] + " LIMIT 10000) df_rita",
+        user=creds["user"],
+        password=creds["password"])\
+    .load()
+# ----------------------------------------------------------------------------------------------------
+
+
+# DEFINICION DE FUNCIONES
+# ----------------------------------------------------------------------------------------------------
+def aeropuerto_demoras_aerolinea(df):
+    """Esta funcion calcula, tomando como referencia el cada aerolínea:
+    - El retraso promedio en la salida de los vuelos.
+    - El retraso promedio en la llegada de los vuelos.
+    - La duración promedio de los vuelos.
+    - El tiempo de rodaje del avión desde el aterrizaje hasta la puerta de desembarco.
+    - El tiempo de rodaje del avión desde la puerta de rodaje hasta el despegue.
+    Los resultados se presentan para cada dia (DAY), cada mes (MONTH), cada trimestre (QUARTER) y cada ano (YEAR)
+    """
+    df_resp = df.rollup('OP_UNIQUE_CARRIER', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH')\
+        .agg(
+            F.count("FL_DATE").alias("FL_DATE"),
+            F.avg('ARR_DELAY').alias("ARR_DELAY"),
+            F.avg('DEP_DELAY').alias("DEP_DELAY"),
+            F.avg('ACTUAL_ELAPSED_TIME').alias("ACTUAL_ELAPSED_TIME"),
+            F.avg('TAXI_IN').alias("TAXI_IN"),
+            F.avg('TAXI_OUT').alias("TAXI_OUT")
+            )
+    return df_resp
+# ----------------------------------------------------------------------------------------------------
+
+
+# EJECUCION
+# ----------------------------------------------------------------------------------------------------
+try:
+	process = config['process']
+except:
+	print('\n\n\t\tNo se definió el proceso que se desea ejecutar.\n\n')
+if process == 'aerolinea':
+	df_resp = aeropuerto_demoras_aerolinea(df_rita) # Calculo de demoras en cada ruta
+else:
+	print('\n\n\tEl nombre del proceso no es válido.\n\n')
+
+df_resp.write.format("jdbc")\
+    .options(
+        url=creds["db_url"] + creds["database"],
+        driver=creds["db_driver"],
+        dbtable=config["results_table"],
+        user=creds["user"],
+        password=creds["password"],
+        numPartitions=config["db_numPartitions"])\
+    .mode(config["results_table_mode"])\
+    .save()
+t_final = time.time() # Tiempo de finalizacion de la ejecucion
+# ----------------------------------------------------------------------------------------------------
+
+
+# REGISTRO DE TIEMPO
+# ----------------------------------------------------------------------------------------------------
+rdd_time = sc.parallelize([[t_inicio, t_final, t_final - t_inicio, config["description"], config["resources"]]]) # Almacenamos infomracion de ejecucion en rdd
+df_time = rdd_time.toDF(['start_ts', 'end_ts', 'duration', 'description', 'resources'])\
+    .withColumn("insertion_ts", F.current_timestamp())
+df_time.write.format("jdbc")\
+    .options(
+        url=creds["db_url"] + creds["database"],
+        driver=creds["db_driver"],
+        dbtable=config["time_table"],
+        user=creds["user"],
+        password=creds["password"])\
+    .mode(config["time_table_mode"])\
+    .save()
+# ----------------------------------------------------------------------------------------------------
