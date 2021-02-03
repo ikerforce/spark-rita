dc64dcedaa8a9dc985ae3084e9046b03528f8557
ikerforce <iker.mt@gmail.com>
2021-02-03 12:39:27 -0600

    Ahora se ejecuta de acuerdo al orden del proceso y añadí manejo de errores.

--- src/rita_ejecuciones.py
+++ src/rita_ejecuciones.py
@@ -3,6 +3,9 @@ import os
 import random
 import argparse # Utilizado para leer archivo de configuracion
 import json # Utilizado para leer archivo de configuracion
+import time
+
+hora_ejecucion = time.strftime("%d_%m_%Y_%H_%M_%S") # 
 
 # Al ejecutar el archivo se debe de pasar el argumento --config /ruta/a/archivo/de/crecenciales.json
 parser = argparse.ArgumentParser()
@@ -23,6 +26,7 @@ def convierte_en_dict(l):
 
 
 def orden_pruebas(numero_de_ejecuciones):
+    '''Esta funcion regresa una lista de tamano 2 * numero_de_ejecuciones de ceros y unos.'''
     pruebas = [1 for i in range(numero_de_ejecuciones)] + [0 for i in range(numero_de_ejecuciones)]
     random.shuffle(pruebas)
     return pruebas
@@ -35,17 +39,27 @@ rutas = lee_config("conf/base/paths_to_file.csv")
 numero_de_ejecuciones = int(args.ejecs)
 procesos = list(rutas.keys())
 pruebas_totales = dict(zip(list(rutas.keys()), [orden_pruebas(numero_de_ejecuciones) for i in range(len(rutas.keys()))]))
+n_errores = 0
+for proceso in procesos:
+    for x in pruebas_totales[proceso]:
+        if x == 1:
+            try:
+                print('\n\t' + proceso + ' spark\n')
+                os.system('spark-submit src/rita_master_spark.py --creds ' + args.creds + ' --conf ' + rutas[proceso]['spark'])
+            except Exception as e:
+                n_errores += 1
+                with open("rita_ejecuciones{fecha}.err".format(fecha=hora_ejecucion), "a") as myfile:
+                    myfile.write('\n\t' + proceso + ' spark\n' + e)
+        else:
+            try:
+                print('\n\t' + proceso + ' dask\n')
+                os.system('python src/rita_master_dask.py --creds ' + args.creds + ' --conf ' + rutas[proceso]['dask'])
+            except Exception as e:
+                n_errores += 1
+                with open("rita_ejecuciones{fecha}.err".format(fecha=hora_ejecucion), "a") as myfile:
+                    myfile.write('\n\t' + proceso + ' dask\n' + e)
 
-print(rutas['origen']['dask'])
-
-while procesos != []:
-    proceso = random.choice(procesos)
-    x = pruebas_totales[proceso].pop()
-    if x == 1:
-        print('\n\t' + proceso + ' spark\n')
-        os.system('spark-submit src/rita_master_spark.py --creds ' + args.creds + ' --conf ' + rutas[proceso]['spark'])
-    else:
-        print('\n\t' + proceso + ' dask\n')
-        os.system('python src/rita_master_dask.py --creds ' + args.creds + ' --conf ' + rutas[proceso]['dask'])
-    if pruebas_totales[proceso] == []:
-        procesos.remove(proceso)
+if n_errores > 0:
+    print('\n\tSe registraron {n_errores} errores.\n'.format(n_errores=n_errores))
+else:
+    print('\n\tNo se registraron errores.')
