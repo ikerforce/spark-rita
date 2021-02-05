b4f63d82cfb8d025975f5cdbdd4f9dc0e3bdf074
ikerforce <iker.mt@gmail.com>
2021-02-04 18:47:55 -0600

    Ya obtiene longitud minima, falta obtener la ruta.

--- src/calculo_ruta_minima/dijkstra_spark.py
+++ src/calculo_ruta_minima/dijkstra_spark.py
@@ -7,6 +7,13 @@ spark = SparkSession(sc)
 sqlContext = SQLContext(sc)
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema = StructType([
+  StructField('ORIGIN', StringType(), True),
+  StructField('DEST', StringType(), True),
+  StructField('W', IntegerType(), True),
+  StructField('R_min', IntegerType(), True)])
 
 def actualiza_peso(nodo_actual, nodo_destino, peso_acumulado, peso_arista, peso_actual):
 	'''Esta funcion actualiza el peso de la arista cuando es necesario.
@@ -16,12 +23,12 @@ def actualiza_peso(nodo_actual, nodo_destino, peso_acumulado, peso_arista, peso_
 	peso_arista - Peso correspondiente a la arista (nodo_actual nodo_destino).
 	peso_actual - El peso actual del nodo inicial hasta el nodo destino.'''
 	if nodo_actual== nodo_destino:
-		if peso_acumulado + peso_arista < peso_actual:
-			return peso_acumulado + peso_arista
+		if int(peso_acumulado) + int(peso_arista) < int(peso_actual):
+			return int(peso_acumulado) + int(peso_arista)
 		else:
-			return peso_actual
+			return int(peso_actual)
 	else:
-		return peso_actual
+		return int(peso_actual)
 
 udf_actualiza_peso = F.udf(actualiza_peso)
 
@@ -38,34 +45,39 @@ pesos_min = [0] + [infinity for i in range(len(pesos)-1)]
 nodos = set(origenes + destinos)
 
 aristas = [x for x in zip(origenes, destinos, pesos, pesos_min)]
-df = sc.parallelize(aristas).toDF(['ORIGIN', 'DEST', 'W', 'R_min'])
+
+df = sc.parallelize(aristas).toDF(schema=schema)
 df_minimal = sc.parallelize([[nodo_actual, peso_actual]]).toDF(['NODO', 'R_min'])
 
 df.show()
 
 # Asignacion de peso
 
-# while df.count().collect()[0][0]:
+while df.count() > 0:
+	# Actualizo el peso de las aristas al minimo posible
+	df = df.withColumn('R_min', udf_actualiza_peso(F.lit(nodo_actual), F.col('ORIGIN'), F.lit(peso_actual), F.col('W'), F.col('R_min')))
+	df.show()
 
-# Actualizo el peso de las aristas al minimo posible
-df = df.withColumn('R_min', udf_actualiza_peso(F.lit(nodo_actual), F.col('ORIGIN'), F.lit(peso_actual), F.col('W'), F.col('R_min')))
-df.show()
+	# Elimino los registros que llevan al nodo en el que estoy parado
+	df = df.filter(F.col('DEST') != F.lit(nodo_actual))
 
-# Elimino los registros que llevan al nodo en el que estoy parado
-df = df.filter(F.col('DEST') != F.lit(nodo_actual))
-df.show()
+	# Calculo el valor minimo de los pesos para obtener el siguiente nodo
+	minimo = df.agg(F.min(F.expr('CAST(R_min AS INT)')).alias('MIN')).collect()[0][0]
 
-# Calculo el valor minimo de los pesos para obtener el siguiente nodo
-minimo = df.agg(F.min(F.expr('CAST(R_min AS INT)')).alias('MIN')).collect()[0][0]
+	print(minimo)
 
-df.show()
+	df.show()
 
-estado_actual = df.filter(F.col('ORIGIN') == F.lit(nodo_actual)).filter(F.col('R_min') == F.lit(minimo)).select('DEST', 'R_min').collect()[0]
-nodo_actual = estado_actual[0]
-peso_actual = estado_actual[1]
-df_minimal = df_minimal.union(sc.parallelize([[nodo_actual, peso_actual]]).toDF(['NODO', 'R_min']))
+	estado_actual = df.filter(F.col('R_min').cast(IntegerType()) == F.lit(minimo).cast(IntegerType()))
+	estado_actual.show()
+	estado_actual = estado_actual.select('DEST', 'R_min').collect()[0]
+	nodo_actual = estado_actual[0]
+	peso_actual = estado_actual[1]
+	print(nodo_actual)
+	print(peso_actual)
+	df_minimal = df_minimal.union(sc.parallelize([[nodo_actual, peso_actual]]).toDF(['NODO', 'R_min']))
 
-df_minimal.show()
+	df_minimal.show()
 
 # actual.show()
 
