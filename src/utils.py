0df4427e4d6f6f02122d4538fe8b74cf5e2d07ac
ikerforce <iker.mt@gmail.com>
2021-03-09 18:29:56 -0600

    Agregué el argumento metadata y cambié la forma de imprimir la excepción.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 # Importaciones necesarias
 import dask.dataframe as dd
 import time
+import pandas as pd
 
 nunique = dd.Aggregation('nunique', lambda s: s.nunique(), lambda s0: s0.sum()) # Definimos como hacer la agregacion para contar elementos únicos
 
@@ -14,7 +15,7 @@ def tiempo_ejecucion(t_inicial):
     tiempo['segundos'] = tiempo_segundos % 3600 % 60
     return tiempo
 
-def unir_columnas(df, col1, col2, col_resultante):
+def unir_columnas(df, col1, col2, col_resultante, meta=pd.DataFrame):
     """Esta función recibe como argumento un dataframe y dos columnas (col1, col2) cuyos valores concatenará en una columna nueva cuyo resultado
     corresponde al cuarto argumento. (col resultante)"""
     df[col_resultante] = df.apply(lambda row: str(row[col1]) + '-' + str(row[col2]), axis=1, meta='str')
@@ -24,20 +25,22 @@ def conjuntos_rollup(columnas): # Hay que ver la forma de que se haga la agregac
     conjuntos = list(map(lambda x: columnas[0:x+1], range(len(columnas))))
     return conjuntos
 
-def group_by_rollup(df, columnas_agregacion, columnas_totales, agregaciones):
+def group_by_rollup(df, columnas_agregacion, columnas_totales, agregaciones, meta=pd.DataFrame):
     columnas_nulas = [item for item in columnas_totales if item not in columnas_agregacion]
     try:
         resultado = df.groupby(columnas_agregacion).agg(agregaciones).reset_index().compute()
-    except:
+    except Exception as e:
         if 'nunique' in list(agregaciones.values()):
             resultado = df.groupby(columnas_agregacion).agg(nunique).reset_index().compute()
         else:
-            print("\nHubo un error en la agregación.")
+            print("\nHubo un error en la agregación.\n")
+            print(e)
+            print('\n')
     for columna in columnas_nulas:
         resultado[columna] = None
     return resultado.drop(columns=['X'])
 
-def rollup(df, columnas, agregaciones):
+def rollup(df, columnas, agregaciones, meta=list):
     df = df[list(set(columnas + list(agregaciones.keys())))]
     df['X'] = 1 # Esta es una columna temporal para hacer la agregación total
     columnas_totales = ["X"] + columnas  # Esta es una columna temporal que se utiliza para la agregacion total.
