c21ae0ecb9f0ae0d04049aab2dd83e0ea6b19537
ikerforce <iker.mt@gmail.com>
2021-04-01 10:22:29 -0600

    Ahora las funciones las obtiene de utils.py

--- src/rita_master_dask.py
+++ src/rita_master_dask.py
@@ -4,7 +4,7 @@ from dask.distributed import Client
 
 if __name__ == '__main__':
 
-    client = Client(n_workers=2)
+    client = Client(n_workers=4)
 
     # Importaciones de Python
     import argparse # Utilizado para leer archivo de configuracion
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     from os import getcwdb # Nos permite conocer el directorio actual
     curr_path = getcwdb().decode() # Obtenemos el directorio actual
     sys.path.insert(0, curr_path) # Agregamos el directioro en el que se encuentra el directorio src
-    # from src import utils # Estas son las funciones definidas por mi
+    from src import utils # Estas son las funciones definidas por mi
 
     # Al ejecutar el archivo se debe de pasar el argumento --config /ruta/a/archivo/de/crecenciales.json
     parser = argparse.ArgumentParser()
@@ -25,69 +25,12 @@ if __name__ == '__main__':
     parser.add_argument("--sample_size", help="Tamaño de la muestra de datos que se utilizará.")
     parser.add_argument("--creds", help="Ruta hacia archivo con credenciales de la base de datos.")
     args = parser.parse_args()
-    # Leemos las credenciales de la ruta especificada
 
-    def lee_config_csv(path, sample_size, process):
-        file = open(path, "r").read().splitlines()
-        nombres = file[0]
-        info = filter(lambda row: row.split("|")[0] == sample_size and row.split("|")[1] == process, file[1:])
-        parametros = dict(zip(nombres.split('|'), list(info)[0].split('|')))
-        return parametros
-
-    config = lee_config_csv(path="conf/base/configs.csv", sample_size=args.sample_size, process=args.process)
+    config = utils.lee_config_csv(path="conf/base/configs.csv", sample_size=args.sample_size, process=args.process)
     with open(args.creds) as json_file:
         creds = json.load(json_file)
 
     uri = 'mysql+pymysql://{0}:{1}@localhost:{2}/{3}'.format(creds["user"], creds["password"], "3306", creds["database"])
-    # ----------------------------------------------------------------------------------------------------
-
-
-    # DEFINICION DE FUNCIONES
-    # ----------------------------------------------------------------------------------------------------
-    nunique = dd.Aggregation('nunique', lambda s: s.nunique(), lambda s0: s0.sum()) # Definimos como hacer la agregacion para contar elementos únicos
-
-    def tiempo_ejecucion(t_inicial):
-        """Esta función mide el tiempo transcurrido entre t_inicial y el momento en el que se llama la función.
-        El resultado es un JSON con los campos: horas, minutos y segundos."""
-        tiempo_segundos = time.time() - t_inicial
-        tiempo = {}
-        tiempo['horas'] = int(tiempo_segundos // 3600)
-        tiempo['minutos'] = int(tiempo_segundos % 3600 // 60)
-        tiempo['segundos'] = tiempo_segundos % 3600 % 60
-        return tiempo
-
-    def unir_columnas(df, col1, col2, col_resultante, meta=pd.DataFrame):
-        """Esta función recibe como argumento un dataframe y dos columnas (col1, col2) cuyos valores concatenará en una columna nueva cuyo resultado
-        corresponde al cuarto argumento. (col resultante)"""
-        df[col_resultante] = df.apply(lambda row: str(row[col1]) + '-' + str(row[col2]), axis=1, meta='str')
-        return df.drop(col1, axis=1).drop(col2, axis=1)
-
-    def conjuntos_rollup(columnas): # Hay que ver la forma de que se haga la agregación total
-        conjuntos = list(map(lambda x: columnas[0:x+1], range(len(columnas))))
-        return conjuntos
-
-    def group_by_rollup(df, columnas_agregacion, columnas_totales, agregaciones, meta=pd.DataFrame):
-        columnas_nulas = [item for item in columnas_totales if item not in columnas_agregacion]
-        try:
-            resultado = df.groupby(columnas_agregacion).agg(agregaciones).reset_index().compute()
-        except Exception as e:
-            if 'nunique' in list(agregaciones.values()):
-                resultado = df.groupby(columnas_agregacion).agg(nunique).reset_index().compute()
-            else:
-                print("\nHubo un error en la agregación.\n")
-                print(e)
-                print('\n')
-        for columna in columnas_nulas:
-            resultado[columna] = None
-        return resultado.drop(columns=['X'])
-
-    def rollup(df, columnas, agregaciones, meta=list):
-        df = df[list(set(columnas + list(agregaciones.keys())))]
-        df['X'] = 1 # Esta es una columna temporal para hacer la agregación total
-        columnas_totales = ["X"] + columnas  # Esta es una columna temporal que se utiliza para la agregacion total.
-        conjuntos_columnas = conjuntos_rollup(columnas_totales)
-        dataframes = list(map(lambda X: group_by_rollup(df, X, columnas_totales, agregaciones), conjuntos_columnas))
-        return dataframes
     # ----------------------------------------------------------------------------------------------------
 
 
@@ -125,24 +68,24 @@ if __name__ == '__main__':
     print('\n\n\tLos resultados se escribirán en la tabla: ' + process + '\n\n')
     if process == 'demoras_aerolinea_dask':
         agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
-        lista_df = rollup(df, ['OP_UNIQUE_CARRIER', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['OP_UNIQUE_CARRIER', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     elif process == 'demoras_aeropuerto_origen_dask':
         agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
-        lista_df = rollup(df, ['ORIGIN', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['ORIGIN', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     elif process == 'demoras_aeropuerto_destino_dask':
         agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
-        lista_df = rollup(df, ['DEST', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['DEST', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     elif process == 'demoras_ruta_aeropuerto_dask':
-        df = unir_columnas(df, "ORIGIN", "DEST", "ROUTE_AIRPORTS")
+        df = utils.unir_columnas(df, "ORIGIN", "DEST", "ROUTE_AIRPORTS")
         agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
-        lista_df = rollup(df, ['ROUTE_AIRPORTS', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['ROUTE_AIRPORTS', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     elif process == 'demoras_ruta_mktid_dask':
-        df = unir_columnas(df, "ORIGIN_CITY_MARKET_ID", "DEST_CITY_MARKET_ID", "ROUTE_MKT_ID")
+        df = utils.unir_columnas(df, "ORIGIN_CITY_MARKET_ID", "DEST_CITY_MARKET_ID", "ROUTE_MKT_ID")
         agregaciones = {'FL_DATE':'count', 'ARR_DELAY':'mean', 'DEP_DELAY':'mean', 'ACTUAL_ELAPSED_TIME':'mean', 'TAXI_IN':'mean', 'TAXI_OUT':'mean'}
-        lista_df = rollup(df, ['ROUTE_MKT_ID', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['ROUTE_MKT_ID', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     elif process == 'flota_dask':
         agregaciones = {'TAIL_NUM' : 'nunique'}
-        lista_df = rollup(df, ['OP_UNIQUE_CARRIER', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
+        lista_df = utils.rollup(df, ['OP_UNIQUE_CARRIER', 'YEAR', 'QUARTER', 'MONTH', 'DAY_OF_MONTH'], agregaciones)
     else:
         print('\n\n\tEl nombre del proceso: ' + process + ' no es válido.\n\n')
 
