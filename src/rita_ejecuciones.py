01d36623ce0ee575be3f0b3f015a678e883eca38
ikerforce <iker.mt@gmail.com>
2021-04-22 18:13:00 -0500

    Corregí comando de dijkstra y añadí conteo de procesos para ver avance.

--- src/rita_ejecuciones.py
+++ src/rita_ejecuciones.py
@@ -41,21 +41,13 @@ def obten_procesos(path, sample_size):
     procesos = set(x.replace('_dask', '').replace('_spark','') for x in map(lambda row: row.split('|')[1], file_sample_only))
     return procesos
 
-def selecciona_aeropuertos_fecha(lista):
-    primero = random.choice(lista)
-    segundo = random.choice(lista)
-    while primero == segundo:
-        segundo = random.choice(lista)
+def selecciona_aeropuertos(lista):
+    origen = random.choice(lista)
+    destino = random.choice(lista)
+    while origen == destino:
+        destino = random.choice(lista)
 
-    start_date = datetime.date(2008, 1, 1)
-    end_date = datetime.date(2020, 11, 30)
-
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days=random_number_of_days)
-
-    return [primero, segundo, str(random_date)]
+    return [origen, destino]
 
 procesos = obten_procesos(path='conf/base/configs.csv', sample_size=args.sample_size)
 
@@ -65,8 +57,13 @@ numero_de_ejecuciones = int(args.ejecs)
 procesos.remove('dijkstra')
 pruebas_totales = dict(zip(procesos, [orden_pruebas(numero_de_ejecuciones) for i in range(len(procesos))]))
 n_errores = 0
+conteo_proceso = 0
+total_p = len(procesos) + 1
+n_total_ejecuciones = total_p * numero_de_ejecuciones * 2
 for proceso in procesos:
     for x in pruebas_totales[proceso]:
+        conteo_proceso += 1
+        print('\tAvance {cp}/{n_ejecs}'.format(cp=conteo_proceso, n_ejecs=n_total_ejecuciones))
         if x == 1:
             try:
                 print('+----------------------------------+')
@@ -107,7 +104,7 @@ pruebas_rutas = orden_pruebas(numero_de_ejecuciones)
 random.shuffle(pruebas_rutas)
 aeropuertos = ['DFW','LAX','MEM','DTW','ORD','IAH','JFK','SFO','MIA','EWR','HNL','LGA','CLT','PHX','BOS','IAD','OGG','AUS','DEN','DCA','ATL','MCO','GSP','ABE','TUL','EGE','FLL','CLE','SBN','SJU','BNA','LAS','BGR','MSP','AVP','SEA','SAN','TUS','MSY','BDL','SNA','CID','CMH','STL','RDU','HPN','PBI','PIT','CHS','FWA']
 # aeropuertos = ['BGM', 'DLG', 'INL', 'PSE', 'MSY', 'PPG', 'GEG', 'DRT', 'SNA', 'BUR', 'GRB', 'GTF', 'IFP', 'IDA', 'GRR', 'LWB', 'JLN', 'PVU', 'EUG', 'PSG', 'ATY', 'PVD', 'MYR', 'GSO', 'OAK', 'EAR', 'FAR', 'MSN', 'BTM', 'FSM', 'COD', 'MQT', 'SCC', 'ESC', 'FNL', 'DCA', 'RFD', 'MLU', 'CID', 'SWO', 'GTR', 'LWS', 'PIB', 'UTM', 'WRG', 'HLN', 'CIU', 'IAG', 'DDC', 'RDM', 'LEX', 'FLO', 'JMS', 'ORF', 'SCE', 'KTN', 'EVV', 'ENV', 'CRW', 'CWA', 'OGS', 'VCT', 'SAV', 'GCK', 'TRI', 'CDV', 'CMH', 'MOD', 'SPN', 'YNG', 'LBF', 'MWH', 'CAK', 'TYR', 'ADK', 'CDB', 'CHO', 'MOB', 'PNS', 'MCN', 'DIK', 'CEC', 'LIH', 'IAH', 'HNL', 'SLN', 'SHV', 'ERI', 'GST', 'CVG', 'SJC', 'TOL', 'LGA', 'BUF', 'TLH', 'CDC', 'ACT', 'HPN', 'RDD', 'AUS', 'MLI', 'GCC', 'SJU', 'DHN', 'ATW', 'LGB', 'GJT', 'AVL', 'LYH', 'CNY', 'GFK', 'BFL', 'RIW', 'RNO', 'SRQ', 'EYW', 'SBN', 'BJI', 'TTN', 'JAC', 'RST', 'CHS', 'HGR', 'RSW', 'TUL', 'HRL', 'IPL', 'ISP', 'AMA', 'BOS', 'MAF', 'MLB', 'TUP', 'EWR', 'LAS', 'BIS', 'OGD', 'JAN', 'FAI', 'ITO', 'IMT', 'UIN', 'XNA', 'HHH', 'DLH', 'HYA', 'DEN', 'EWN', 'RHI', 'SGU', 'ALS', 'ALB', 'CPR', 'LNK', 'OME', 'GRI', 'PSP', 'SBA', 'BOI', 'IAD', 'IYK', 'MEI', 'LAR', 'HOB', 'DRO', 'BRO', 'BRD', 'BMI', 'RKS', 'SEA', 'LAN', 'CMI', 'VEL', 'LRD', 'PBG', 'HYS', 'VLD', 'PSM', 'MCI', 'PIR', 'FLG', 'GRK', 'CLT', 'TVC', 'BNA', 'CLL', 'CGI', 'PAE', 'UST', 'PSC', 'BLI', 'CIC', 'ORH', 'ABQ', 'PBI', 'PIE', 'SDF', 'ART', 'ACV', 'LAW', 'SCK', 'SLE', 'BDL', 'DAL', 'MRY', 'ITH', 'DBQ', 'USA', 'CLE', 'PDX', 'ACK', 'MIA', 'MFR', 'TWF', 'ILG', 'TPA', 'BWI', 'BKG', 'APN', 'CMX', 'PRC', 'OKC', 'ROA', 'SMF', 'SPI', 'BRW', 'OTH', 'SFB', 'BFF', 'ABI', 'OXR', 'MBS', 'ELM', 'PHX', 'ABR', 'DVL', 'FCA', 'STL', 'ABY', 'PWM', 'BET', 'DFW', 'MHT', 'ABE', 'TXK', 'GSP', 'LSE', 'BFM', 'MMH', 'FAY', 'STX', 'HDN', 'EFD', 'GUC', 'LMT', 'LBB', 'EKO', 'CRP', 'HVN', 'EGE', 'FSD', 'SWF', 'BQK', 'SUN', 'CSG', 'SFO', 'MEM', 'SAF', 'ELP', 'BHM', 'ATL', 'FLL', 'FNT', 'PIH', 'YKM', 'AZA', 'DEC', 'RIC', 'AKN', 'LCK', 'DAY', 'PHF', 'OMA', 'SJT', 'LCH', 'STC', 'TEX', 'VPS', 'BPT', 'MHK', 'TKI', 'LIT', 'MVY', 'FAT', 'ICT', 'CAE', 'ECP', 'PFN', 'ORD', 'AVP', 'LBL', 'LBE', 'BTV', 'COU', 'MKG', 'IPT', 'BIL', 'AEX', 'SPS', 'ILM', 'SMX', 'PUB', 'PIA', 'GUM', 'RDU', 'BQN', 'PGV', 'MFE', 'HIB', 'MKE', 'XWA', 'SYR', 'BLV', 'ISN', 'HSV', 'LFT', 'PIT', 'MTJ', 'TUS', 'ROW', 'ACY', 'MDW', 'AZO', 'PLN', 'COS', 'CKB', 'OAJ', 'JNU', 'IND', 'ALO', 'KOA', 'EAU', 'GPT', 'MGM', 'OWB', 'DTW', 'TYS', 'HOU', 'CHA', 'YUM', 'ADQ', 'MDT', 'ONT', 'FWA', 'JAX', 'STS', 'LAX', 'MSP', 'MOT', 'HTS', 'BTR', 'BGR', 'SIT', 'MCO', 'SGF', 'AGS', 'ROC', 'OTZ', 'WYS', 'SAN', 'BZN', 'GGG', 'SHD', 'YAK', 'JFK', 'DAB', 'ANC', 'PAH', 'SUX', 'MSO', 'GNV', 'CYS', 'PHL', 'OGG', 'PGD', 'DSM', 'FOE', 'SAT', 'SLC', 'PMD', 'SHR', 'STT', 'SBP', 'RAP', 'ASE', 'CLD']
-rutas_spark = [selecciona_aeropuertos_fecha(aeropuertos) for i in range(numero_de_ejecuciones)]
+rutas_spark = [selecciona_aeropuertos(aeropuertos) for i in range(numero_de_ejecuciones)]
 rutas_dask = rutas_spark.copy()
 
 
@@ -125,8 +122,7 @@ for i in pruebas_rutas:
                         --process {process} \
                         --creds {creds} \
                         --origin {origin} \
-                        --dest {dest} \
-                        --dep_date {date}'''.format(origin=ruta[0], dest=ruta[1], date=ruta[2], sample_size=args.sample_size, process='dijkstra_spark', creds=args.creds)
+                        --dest {dest}'''.format(origin=ruta[0], dest=ruta[1], sample_size=args.sample_size, process='dijkstra_spark', creds=args.creds)
         os.system(spark_cmd)
         print('+----------------------------------+')
     else:
@@ -139,8 +135,7 @@ for i in pruebas_rutas:
                         --process {process} \
                         --creds {creds} \
                         --origin {origin} \
-                        --dest {dest} \
-                        --dep_date {date}'''.format(origin=ruta[0], dest=ruta[1], date=ruta[2], sample_size=args.sample_size, process='dijkstra_dask', creds=args.creds)
+                        --dest {dest}'''.format(origin=ruta[0], dest=ruta[1], sample_size=args.sample_size, process='dijkstra_dask', creds=args.creds)
         os.system(dask_cmd)
         print('+----------------------------------+')
 
