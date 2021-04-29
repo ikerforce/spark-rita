c9a04c9792be25c758b70c6a540addf39e7b4eb1
Ubuntu <sshuser@hn0-rita-d.2hs5k34or2duxdj2n3kjcmwwqa.jx.internal.cloudapp.net>
2021-04-29 16:49:16 +0000

    Actualicé el script para que lea de hdfs

--- dask_test.py
+++ dask_test.py
@@ -1,18 +1,21 @@
+import os
+os.environ['ARROW_LIBHDFS_DIR'] = '/usr/hdp/4.1.4.0/'
+
 from dask_yarn import YarnCluster
 from dask.distributed import Client
 import dask.dataframe as dd # Utilizado para el procesamiento de los datos
 
 cluster = YarnCluster(environment='conf/conda_envs/dask_yarn.tar.gz',
                       worker_vcores=2,
-                      worker_memory="8GiB")
+                      worker_memory="2GiB")
 
-cluster.scale(2)
+cluster.scale(1)
 
 # if __name__ == '__main__':
 
 client = Client(cluster)
-path = 'hdfs:///data_10K_dask_casted/'
+path = 'hdfs:///samples/data_100K_dask_casted/data_100K_dask_casted'
 
-df = dd.read_parquet(path, engine='pyarrow')
+df = dd.read_parquet(path)
 
 print(df.count().compute())
