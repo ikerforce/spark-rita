2013986df46632b53e7f6171652f35929bab8217
Ubuntu <sshuser@hn0-rita-d.2hs5k34or2duxdj2n3kjcmwwqa.jx.internal.cloudapp.net>
2021-04-29 17:54:59 +0000

    Puse print

--- dask_test.py
+++ dask_test.py
@@ -6,15 +6,21 @@ from dask.distributed import Client
 import dask.dataframe as dd # Utilizado para el procesamiento de los datos
 
 cluster = YarnCluster(environment='conf/conda_envs/dask_yarn.tar.gz',
-                      worker_vcores=2,
-                      worker_memory="2GiB")
+                      worker_vcores=4,
+                      worker_memory="2GB")
 
-cluster.scale(1)
+print(cluster.application_client.get_containers())
+
+cluster.scale(2)
+
+print(cluster)
 
 # if __name__ == '__main__':
 
 client = Client(cluster)
 path = 'hdfs:///samples/data_100K_dask_casted/data_100K_dask_casted'
+
+print(client)
 
 df = dd.read_parquet(path)
 
