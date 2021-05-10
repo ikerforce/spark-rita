d67af9a2249ac15eb013242a450ddf2ae2eb02f7
ikerforce <iker.mt@gmail.com>
2021-05-09 20:30:51 -0500

    Agregué variable de entorno de libhdfs

--- conf/dask_cluster/dask_scheduler_prep.sh
+++ conf/dask_cluster/dask_scheduler_prep.sh
@@ -1,5 +1,8 @@
 yum -y install wget
 yum -y install git
+sudo echo 'export ARROW_LIBHDFS_DIR="/usr/hdp/4.1.4.0"' >> ~/.bashrc
+sudo echo 'export PATH="$ARROW_LIBHDFS_DIR:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 git clone https://ikerforce:$1@github.com/ikerforce/spark-rita.git
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
